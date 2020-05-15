2aa8ccf525522c735230b962e0cb94978bc78534
J Guzman <jmguzmanus@gmail.com>
2020-05-15 15:04:13 -0400

    Readded prettierrc config file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import Layout from "./hoc/Layout/Layout";
-import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from "./containers/Checkout/Checkout";
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-          <Checkout />
-        </Layout>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <BurgerBuilder />
+                    <Checkout />
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
