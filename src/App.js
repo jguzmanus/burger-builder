f895170bb94cfaf37a3766d11b300177c0dbde30
J Guzman <jmguzmanus@gmail.com>
2020-06-22 20:41:55 -0400

    Added initial Auth form.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import { Route, Switch } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
     render() {
@@ -12,19 +13,10 @@ class App extends Component {
             <div>
                 <Layout>
                     <Switch>
-                        <Route
-                            path="/checkout"
-                            component={Checkout}
-                        />
-                        <Route
-                            path="/orders"
-                            component={Orders}
-                        />
-                        <Route
-                            path="/"
-                            exact
-                            component={BurgerBuilder}
-                        />
+                        <Route path="/checkout" component={Checkout} />
+                        <Route path="/orders" component={Orders} />
+                        <Route path="/auth" component={Auth} />
+                        <Route path="/" exact component={BurgerBuilder} />
                     </Switch>
                 </Layout>
             </div>
---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,127 @@
+import classes from './Auth.module.css';
+import React, { Component } from 'react';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+import * as actions from '../../store/actions/index';
+import { connect } from 'react-redux';
+
+class Auth extends Component {
+    state = {
+        controls: {
+            email: {
+                ...this.generateAttributes('input', 'email', 'Your Email', ''),
+                validation: { required: true, isEmail: true },
+            },
+            password: {
+                ...this.generateAttributes('input', 'password', 'Password', ''),
+                validation: { required: true, minLength: 6 },
+            },
+        },
+    };
+
+    /******************************************************************/
+
+    validateData(value, rules) {
+        let isValid = true;
+
+        if (rules.required) isValid = value.trim() !== '' && isValid;
+
+        if (rules.minLength)
+            isValid = value.trim().length >= rules.minLength && isValid;
+        if (rules.maxLength)
+            isValid = value.trim().length <= rules.maxLength && isValid;
+
+        if (rules.isEmail) {
+            const pattern = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/g;
+            isValid = pattern.test(value) && isValid;
+        }
+
+        return isValid;
+    }
+
+    /******************************************************************/
+
+    generateAttributes(elementType, type, placeholder, value) {
+        return {
+            elementType: elementType,
+            elementConfig: {
+                type: type,
+                placeholder: placeholder,
+            },
+            value: value,
+            valid: false,
+            touched: false,
+        };
+    }
+    /******************************************************************/
+
+    inputChangedHandler = (event, inputElementId) => {
+        const updatedControls = { ...this.state.controls };
+        const updatedControl = {
+            ...updatedControls[inputElementId],
+        };
+
+        updatedControl.value = event.target.value;
+
+        updatedControl.valid = this.validateData(
+            updatedControl.value,
+            updatedControl.validation
+        );
+
+        updatedControl.touched = true;
+
+        updatedControls[inputElementId] = updatedControl;
+
+        this.setState({
+            controls: updatedControls,
+        });
+    };
+
+    /******************************************************************/
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(
+            this.state.controls.email.value,
+            this.state.controls.password.value
+        );
+    };
+    /******************************************************************/
+    render() {
+        let formElements = [];
+
+        for (let key in this.state.controls)
+            formElements.push({
+                id: key,
+                config: this.state.controls[key],
+            });
+
+        let form = (
+            <form onSubmit={this.submitHandler}>
+                {formElements.map((e) => (
+                    <Input
+                        key={e.id}
+                        elementType={e.config.elementType}
+                        elementConfig={e.config.elementConfig}
+                        value={e.config.value}
+                        invalid={!e.config.valid}
+                        requiresValidation={e.config.validation}
+                        touched={e.config.touched}
+                        changed={(event) => this.inputChangedHandler(event, e.id)}
+                    />
+                ))}
+                <Button btnType="Success">SUBMIT</Button>
+            </form>
+        );
+
+        return <div className={classes.Auth}>{form}</div>;
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password)),
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
