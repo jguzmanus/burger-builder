5fc8233c6841050d449cf3e296e2ca2a54ed3312
J Guzman <jmguzmanus@gmail.com>
2020-12-09 16:33:34 -0500

    Updating UI depending on Auth State

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,13 +3,16 @@ import React from 'react';
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => (
+const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem exact link="/" active>
             Burger Builder
         </NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
-        <NavigationItem link="/auth">Authenticate</NavigationItem>
+        { props.isAuth 
+            ? <NavigationItem link="/logout">Logout</NavigationItem>
+            : <NavigationItem link="/auth">Authenticate</NavigationItem>
+        }
     </ul>
 );
 
